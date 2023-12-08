ac72d83d41a9502616bd5f2632a3fac9933ceaab
Emotional_bug <1541373309@qq.com>
2023-12-08 09:57:55 +0800

    grpc文件

--- auth-service/router/auth/route.go
+++ auth-service/router/auth/route.go
@@ -10,15 +10,24 @@ type AuthRouter struct {
 
 func (*AuthRouter) Route(r *gin.Engine) {
 	auth := handle.New()
+
 	//功能路由
 	r.GET("/code", auth.PostEmailCode)
 	r.POST("/file", auth.UploadPic)
+
 	//登录注册
 	r.POST("/login", auth.UserLogin)
 	r.POST("/register", auth.UserRegister)
-	//个人信息
+
+	//个人中心
 	r.GET("/getInfo", auth.GetUserInfo)
 	r.POST("/setInfo", auth.SetUserInfo)
-	//账号信息相关
 	r.POST("/setAccount", auth.SetAccount)
+
+	//用户管理
+	//Certification 用于从token获取信息
+	r.GET("/getUserList", Certification(), auth.GetUserList)
+	r.POST("/setUserRole", Certification(), auth.SetUserRole)
+	r.POST("/setUserAuth", Certification(), auth.SetUserAuthority)
+	r.POST("/deleteUser", Certification(), auth.DeleteUser)
 }
