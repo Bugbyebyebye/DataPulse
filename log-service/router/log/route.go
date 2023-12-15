e5f08de9f35398ac53d32ee2ee3429108e500f68
sora <github@resoras.com>
2023-12-15 13:37:08 +0800

    实现日志记录/接口

--- log-service/router/log/route.go
+++ log-service/router/log/route.go
@@ -10,6 +10,12 @@ type LogRouter struct {
 
 func (*LogRouter) Route(r *gin.Engine) {
 	log := handle.New()
-
+	//Todo
 	r.GET("/getLog", log.GetLogInfo)
+	//前端获取路由
+	r.POST("/deleteuserlogs", log.DeleteUserLogs)
+	r.POST("/deleteonelogs", log.DeleteOneLogs)
+	r.POST("/getuserlogs", log.GetUserLogs)
+	//断点推送路由
+	r.POST("/recorduserlog", log.Logging)
 }
