866375b15a0c043986073dd9e487584ae629f772
sora <github@resoras.com>
2023-12-18 14:57:14 +0800

    数据仓库代码更新/构建

--- store-service/service/bottom.go
+++ store-service/service/bottom.go
@@ -12,21 +12,21 @@ func GetBottomTableInfo(ctx *gin.Context) []common.Table {
 	var result []common.Table
 
 	var first []common.Table
-	err := requests.URL("http://localhost:8085").
+	err := requests.URL("http://mysql-first:8085").
 		Path("/getInfo").
 		ToJSON(&first).Fetch(ctx)
 	if err != nil {
 		log.Printf("err => %+v", err)
 	}
 	var second []common.Table
-	err = requests.URL("http://localhost:8086").
+	err = requests.URL("http://mysql-second:8086").
 		Path("/getInfo").
 		ToJSON(&second).Fetch(ctx)
 	if err != nil {
 		log.Printf("err => %+v", err)
 	}
 	var three []common.Table
-	err = requests.URL("http://localhost:8087").
+	err = requests.URL("http://mongodb-first:8087").
 		Path("/getInfo").
 		ToJSON(&three).Fetch(ctx)
 	if err != nil {
