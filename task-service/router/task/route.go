b65fbdc04ef89efdb5b65fddda9e6dfb1638c31f
sora <github@resoras.com>
2023-12-25 22:00:38 +0800

    修复拉取API列表Bug

--- task-service/router/task/route.go
+++ task-service/router/task/route.go
@@ -10,8 +10,11 @@ type TaskRouter struct {
 
 func (*TaskRouter) Route(r *gin.Engine) {
 	task := handle.New()
-
+	//处理服务启停止相关
 	r.POST("/docker/run", task.RunDocker)
 	r.GET("/docker/stop", task.StopDocker)
 	r.GET("/docker/restart", task.RestartDocker)
+	//处理用户操作相关
+	r.GET("/getuserapilist", task.SearchAPIList)
+	r.POST("/deleteapilist", task.DeleteApi)
 }
