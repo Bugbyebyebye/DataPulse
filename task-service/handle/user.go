a0355e8e99f09e2df1a09d6b15ffc63cd8b09595
sora <github@resoras.com>
2023-12-28 22:43:13 +0800

    修改接口两种查询方式

--- task-service/handle/user.go
+++ task-service/handle/user.go
@@ -49,19 +49,43 @@ func (TaskHandle) DeleteApi(ctx *gin.Context) {
 
 func (TaskHandle) SearchStatusLables(ctx *gin.Context) {
 	res := &result.Result{}
-	get := ctx.Request.Header.Get("id")
-	// 尝试将字符串转换为整数
-	UserID, err := strconv.Atoi(get)
-	if err != nil {
-		// 转换失败，处理错误
-		fmt.Println("转换失败:", err)
+	var req map[string]interface{}
+	// 通过 BindJSON 将 JSON 数据绑定到 map 中
+	if err := ctx.BindJSON(&req); err != nil {
+		fmt.Printf("json数据错误 err => %s", err)
+		ctx.JSON(200, res.Fail(400, "json数据错误！"))
+		return
 	}
-	stateCounts, err := model.CountStates(UserID)
-	if err != nil {
-		fmt.Println("查询出错")
-		fmt.Println(err)
+	UserIDCache, ok := req["user_id"].(float64)
+	var UserID int
+	if ok {
+		fmt.Println("断言成功，验证传入了ID")
+		UserID = int(UserIDCache)
+		stateCounts, err := model.CountStates(UserID)
+		if err != nil {
+			fmt.Println("查询出错")
+			fmt.Println(err)
+		}
+		ctx.JSON(200, res.Success(stateCounts))
+		return
+	} else {
+		fmt.Println("断言失败，验证没有传入了ID")
+		fmt.Println(UserID)
+		get := ctx.Request.Header.Get("id")
+		// 尝试将字符串转换为整数
+		UserID, err := strconv.Atoi(get)
+		if err != nil {
+			// 转换失败，处理错误
+			fmt.Println("转换失败:", err)
+		}
+		stateCounts, err := model.CountStates(UserID)
+		if err != nil {
+			fmt.Println("查询出错")
+			fmt.Println(err)
+		}
+		ctx.JSON(200, res.Success(stateCounts))
+		return
 	}
-	ctx.JSON(200, res.Success(stateCounts))
 }
 
 // SearchAPIList 实现拉取api列表的功能
